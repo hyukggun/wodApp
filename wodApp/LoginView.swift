075bfa16bc9a44ad46912e39bae4cc0cfd397895
hyukggun <hyukggunchoe@gmail.com>
2023-08-21 22:43:14 +0900

    Initial Commit

---
+++ wodApp/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  wodApp
+//
+//  Created by 최최광현 on 2023/08/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
