b3cc05fdf8db6033cf416115ff98a7c9f7a6011d
hyukggun <hyukggunchoe@gmail.com>
2023-08-21 22:43:39 +0900

    Add LoginView

--- wodApp/ContentView.swift
+++ wodApp/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var mainImage: UIImage = UIImage()
+    
+    private var screenWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image(uiImage: mainImage)
+                        .resizable()
+                        .frame(alignment: .top)
+                        .aspectRatio(contentMode: .fit)
+                    Spacer()
+                }
+                .edgesIgnoringSafeArea(.all)
+                
+                
+                VStack {
+                    ActionSheet() {
+                        LoginView()
+                    }
+                    .offset(y: mainImage.size.height + 50)
+                    Spacer()
+                }
+                .edgesIgnoringSafeArea(.all)
+                .cornerRadius(10)
+            }
         }
-        .padding()
+        .navigationBarHidden(true)
+        .onAppear {
+            guard let url = URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwV3u84ju4li8YemjAk-SQ7aBBHO4N2TJNhA&usqp=CAU") else {
+                print("url in nil")
+                mainImage = UIImage()
+                return
+            }
+            Task {
+                do {
+                    let (data, response) = try await URLSession.shared.data(from: url)
+                    guard let response = response as? HTTPURLResponse else {
+                        mainImage = UIImage()
+                        return
+                    }
+                    print(response.statusCode)
+                    guard let image = UIImage(data: data) else { return }
+                    mainImage = image
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
@@ -24,3 +69,31 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+private struct ActionSheet: View {
+    @State var destination: () -> LoginView
+    
+    init(destination: @escaping () -> LoginView) {
+        self.destination = destination
+    }
+    
+    var body: some View {
+        VStack(spacing : 15) {
+            Text("Shape Your body!!")
+                .frame(height: 50)
+            Spacer().frame(height: 10)
+            NavigationLink(destination: destination) {
+                Text("Login")
+                    .foregroundColor(Color.pink)
+            }
+            .frame(width: UIScreen.main.bounds.width)
+        }
+        .padding(.bottom, (UIApplication.shared.windows.last?.safeAreaInsets.bottom)! + 10)
+        .padding(.horizontal)
+        .padding(.top, 20)
+        .background(Color.white)
+        .cornerRadius(30)
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
--- wodApp/LoginView.swift
+++ wodApp/LoginView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var id: String = ""
+    @State var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome back to our app!!")
+            VStack {
+                TextField("Id", text: $id)
+                    .padding(.vertical, 10)
+                    .padding(.leading, 10)
+                TextField("password", text: $password)
+                    .padding(.vertical, 10)
+                    .padding(.leading, 10)
+            }
+            .border(Color.red.opacity(0.1), width:  1)
+            .padding(.all, 24)
+            HStack {
+                Button(action:  {
+                    if checkId() {
+                        print("id 통과")
+                    } else {
+                        print("id 통과 x")
+                    }
+                }) {
+                    Text("Login")
+                }
+                .padding(.all, 20)
+                .background(Color.green)
+                .cornerRadius(5)
+                
+                Button(action:  {
+                    print("sign in")
+                }) {
+                    Text("Sign in")
+                }
+                .padding(20)
+                .background(Color.green)
+                .cornerRadius(5)
+            }
+        }
+        .navigationBarBackButtonHidden()
+    }
+    
+    private func checkId() -> Bool {
+        if id.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    private func checkPassword() -> Bool {
+        if password.isEmpty {
+            return false
+        }
+        return password.count >= 12
     }
 }
 
