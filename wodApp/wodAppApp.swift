6a023361aaf43303eb75247ae9eeacc32c6eecbe
hyukggun <hyukggunchoe@gmail.com>
2023-08-21 21:23:37 +0900

    Initial Commit

---
+++ wodApp/wodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  wodAppApp.swift
+//  wodApp
+//
+//  Created by 최최광현 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct wodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
